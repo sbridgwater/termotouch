bba9454ea4200e1024243026ea5533c1f722f124
sbridgwater <simon.bridgwater@yahoo.it>
2019-12-24 12:52:17 +0100

    1.1
    1.1

--- functions/idp.js
+++ functions/idp.js
@@ -9,7 +9,7 @@ const client = new faunadb.Client({
 exports.handler = (event, context, callback) => {
   /* parse the string body into a useable JS object */
   /* const data = JSON.parse(event.body); */
-  const data = "hello";
+  const data = "hello!";
   console.log("Function `idp` invoked", data)
   const todoItem = {
     data: data
