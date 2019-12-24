ca6fd88dc35ea225a6f425749df63f62f9f179db
sbridgwater <simon.bridgwater@yahoo.it>
2019-12-24 17:12:33 +0100

    1
    1

--- functions/idp.js
+++ functions/idp.js
@@ -9,7 +9,7 @@ const querystring = require("querystring");
 
 /* export our lambda function as named "handler" export */
 exports.handler = (event, context, callback) => {
-  const data = querystring.parse(event.body);
+  const data = querystring.parse(event);
   console.log("Function `idp` invoked", data)
   const todoItem = {
     data: data
