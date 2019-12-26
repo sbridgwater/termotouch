e19884d35abcb03c15c7042670f1284ab96725b4
sbridgwater <simon.bridgwater@yahoo.it>
2019-12-26 17:18:26 +0100

    ut
    ut

--- functions/idp.js
+++ functions/idp.js
@@ -9,7 +9,7 @@ const client = new faunadb.Client({
 crypto = require('crypto');
 algorithm = 'aes-256-cbc';
 inputEncoding = 'utf8';
-outputEncoding = 'hex';
+outputEncoding = 'utf8';
 const IV_LENGTH = 16; // For AES, this is always 16
 ENCRYPT_KEY =  process.env.ENCRYPT_KEY; // Must be 32 characters
 
