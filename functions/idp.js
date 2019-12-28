82cf0f635ea483f2869fecf1ef6bab17ecb546b7
sbridgwater <simon.bridgwater@yahoo.it>
2019-12-28 19:50:05 +0100

    crs
    crs

--- functions/idp.js
+++ functions/idp.js
@@ -109,6 +109,11 @@ exports.handler = (event, context, callback) => {
     }
     return callback(null, {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':
+          'Origin, X-Requested-With, Content-Type, Accept',
+      },
       body: JSON.stringify(jsondata)
     })
   }).catch((error) => {
