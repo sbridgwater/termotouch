7a80a1b8c5fa9907cc9fac6d400ea71ff8585472
Simon Bridgwater <simon.bridgwater@yahoo.it>
2021-01-20 12:22:17 +0100

    idp react

--- functions/idp-react.js
+++ functions/idp-react.js
@@ -112,6 +112,8 @@ exports.handler = (event, context, callback) => {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+	'Access-Control-Allow-Methods': '*', 
 	'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify(jsondata)
